dcc1bc24f615da3b35b563631a51cf3283ede89e
TiaWidi <tia.septianawidi@gmail.com>
2025-04-04 22:04:18 +0700

    Project show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_project, only: [:show, :edit, :update, :destroy, :add_comment, :change_status]
+  before_action :set_project, only: [:show, :edit, :update, :destroy, :add_comment, :change_status]
 
   def index
     @projects = Project.all
@@ -12,6 +12,8 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    @activities = @project.activities.includes(:user)
+    @activity = Activity.new
   end
 
   def new
@@ -27,5 +29,21 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def add_comment
+    @project.add_comment(current_user, params[:content])
+    redirect_to @project, notice: 'Comment was successfully added.'
+  end
+  
+  def change_status
+    @project.change_status(current_user, params[:status])
+    redirect_to @project, notice: 'Project status was successfully updated.'
+  end
+
+  private
+  
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
