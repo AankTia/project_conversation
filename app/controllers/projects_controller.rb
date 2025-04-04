f42bdf48ecfe14a2b060b31c895915249233842a
TiaWidi <tia.septianawidi@gmail.com>
2025-04-04 21:49:28 +0700

    Project index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,14 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :set_project, only: [:show, :edit, :update, :destroy, :add_comment, :change_status]
+
   def index
+    @projects = Project.all
+
+    respond_to do |format|
+      format.html # Default - renders index.html.erb
+      format.json { render json: @projects }
+    end
   end
 
   def show
